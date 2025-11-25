b5c84ed68f08a7db648bc79ab6142cad9a953192
Tharun Kumar Reddy B <btharunkumarreddy39@gmail.com>
2025-11-25 12:23:37 +0530

    Update chatbot_node.py

--- src/Nodes/chatbot_node.py
+++ src/Nodes/chatbot_node.py
@@ -1,13 +1,16 @@
-from Tools.tools import nasa_tool, api_tool, satellite_data_tool
 from State.state import State
+
 
 class ChatbotNode:
     def __init__(self, llm):
         self.llm = llm
-    def chatbot(self,tools):
-        """Receives graph state and returns the LLM response."""
-        # user_message = state["messages"]
+
+    def chatbot(self, tools):
         llm_with_tools = self.llm.bind_tools(tools)
-        def chatbot_node(state:State)->dict:
-            return {"messages":llm_with_tools.invoke(state['messages'])}
-        return chatbot_node
+
+        def node(state: State):
+            return {"messages": llm_with_tools.invoke({
+                "messages": state["messages"],
+                "session_id": state["session_id"]
+            })}
+        return node
