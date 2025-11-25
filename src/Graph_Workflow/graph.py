a1d16074be9411c5d3e77e7124b28da18d6b5afd
Tharun Kumar Reddy B <btharunkumarreddy39@gmail.com>
2025-11-25 12:23:00 +0530

    Update graph.py

--- src/Graph_Workflow/graph.py
+++ src/Graph_Workflow/graph.py
@@ -1,29 +1,30 @@
 from langgraph.graph import StateGraph, START, END
-from State.state import State
-from Tools.tools import get_tools, document_tool
 from langgraph.prebuilt import ToolNode, tools_condition
-from Nodes.chatbot_node import ChatbotNode
 from langgraph.checkpoint.memory import MemorySaver
+
+from State.state import State
+from Nodes.chatbot_node import ChatbotNode
+from Tools.tools import get_tools
 
 
 class Graphbuilder:
     def __init__(self, model):
         self.llm = model
         self.memory = MemorySaver()
-        self.graph_builder = StateGraph(State)
+        self.graph = StateGraph(State)
 
     def get_chatbot_graph(self):
         tools = get_tools()
-        tool_node = ToolNode(tools=tools)
+        tool_node = ToolNode(tools)
 
-        chatbot = ChatbotNode(self.llm).chatbot(tools=tools)
+        chatbot = ChatbotNode(self.llm).chatbot(tools)
 
-        self.graph_builder.add_node("chatbot", chatbot)
-        self.graph_builder.add_node("tools", tool_node)
+        self.graph.add_node("chatbot", chatbot)
+        self.graph.add_node("tools", tool_node)
 
-        self.graph_builder.add_edge(START, "chatbot")
-        self.graph_builder.add_conditional_edges("chatbot", tools_condition)
-        self.graph_builder.add_edge("tools", "chatbot")
-        self.graph_builder.add_edge("chatbot", END)
+        self.graph.add_edge(START, "chatbot")
+        self.graph.add_conditional_edges("chatbot", tools_condition)
+        self.graph.add_edge("tools", "chatbot")
+        self.graph.add_edge("chatbot", END)
 
-        return self.graph_builder.compile(checkpointer=self.memory)
+        return self.graph.compile(checkpointer=self.memory)
