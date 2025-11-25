fd315a3afbea09005b96ce2fb87834f8dcabbcb7
Tharun Kumar Reddy B <btharunkumarreddy39@gmail.com>
2025-11-25 12:22:36 +0530

    Update groqllm.py

--- src/LLM/groqllm.py
+++ src/LLM/groqllm.py
@@ -1,8 +1,10 @@
 from langchain_groq import ChatGroq
+
+
 class GroqLLM:
-    def __init__(self,llm,api_key):
+    def __init__(self, llm: str, api_key: str):
         self.llm = llm
         self.api_key = api_key
+
     def load_llm(self):
-        model = ChatGroq(model=self.llm,api_key=self.api_key)
-        return model
+        return ChatGroq(model=self.llm, api_key=self.api_key)
